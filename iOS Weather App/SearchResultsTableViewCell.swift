dfabe0dc24600035573acf1d22964124b2934d04
Jai Nijhawan <>
2023-07-23 13:34:41 -0400

    Completed the UI

---
+++ iOS Weather App/SearchResultsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultsTableViewCell.swift
+//  iOS Weather App
+//
+//  Created by Jai Nijhawan on 23/07/23.
+//
+
+import UIKit
+
+class SearchResultsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cityNameLabel: UILabel!
+
+}
--- iOS Weather App/ViewController.swift
+++ iOS Weather App/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var currentLoactionButton: UIButton!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var searchResultsTableView: UITableView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherTempratureLabel: UILabel!
+    
+    var locationManager: CLLocationManager?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
     }
-
-
+    
+    @IBAction func segmantControlValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func citiesButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func currentLocationButtonTapped(_ sender: Any) {
+        guard let lat = locationManager?.location?.coordinate.latitude,
+              let lon = locationManager?.location?.coordinate.longitude else { return }
+        
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            print(manager.location?.coordinate)
+        }
+    }
+}
