24fbf95936ad6cd76597bf03497516182425b618
Jai Nijhawan <>
2023-07-25 12:08:30 -0400

    Added Current Loaction Weather Data Feature and segment Control

---
+++ iOS Weather App/Network.swift
@@ -0,0 +1,36 @@
+//
+//  Network.swift
+//  iOS Weather App
+//
+//  Created by Jai Nijhawan on 23/07/23.
+//
+
+import Foundation
+
+class Network {
+    
+    private init() { }
+    
+    static let shared = Network()
+    private static let APIKEY = "e0c4b1cdfc844e66a38203457232207"
+    
+    let baseURL = "https://api.weatherapi.com/v1/current.json"
+    func getWeatherDataFor(lat: Double, lon: Double, completion: @escaping (WeatherResponse?, Error?) -> () ) {
+        guard var url = URLComponents(string: baseURL) else { return }
+        url.queryItems = [
+            URLQueryItem(name: "key", value: Network.APIKEY),
+            URLQueryItem(name: "q", value: "\(lat),\(lon)")
+        ]
+        guard let finalURL = url.url?.absoluteURL else { return }
+        URLSession.shared.dataTask(with: finalURL, completionHandler: { data, response, error in
+            guard let jsonData = data else { return }
+            do {
+                let weatherResponse = try JSONDecoder().decode(WeatherResponse.self, from: jsonData)
+                completion(weatherResponse, nil)
+            } catch let parseErr {
+                print("JSON Parsing Error", parseErr)
+                completion(nil, parseErr)
+            }
+        }).resume()
+    }
+}
--- iOS Weather App/ViewController.swift
+++ iOS Weather App/ViewController.swift
@@ -19,26 +19,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var weatherTempratureLabel: UILabel!
     
     var locationManager: CLLocationManager?
-
+    var weatherData: WeatherResponse?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
+        currentLocationButtonTapped(self)
     }
     
     @IBAction func segmantControlValueChanged(_ sender: Any) {
+        guard let segmentContol = sender as? UISegmentedControl,
+              let weatherData = weatherData else { return }
+        if segmentContol.selectedSegmentIndex == 0 {
+            self.weatherTempratureLabel.text = "\(weatherData.current?.tempC ?? 0)"
+        } else {
+            self.weatherTempratureLabel.text = "\(weatherData.current?.tempF ?? 0)"
+        }
     }
     
     @IBAction func citiesButtonTapped(_ sender: Any) {
     }
     
+    
+    
     @IBAction func currentLocationButtonTapped(_ sender: Any) {
         guard let lat = locationManager?.location?.coordinate.latitude,
               let lon = locationManager?.location?.coordinate.longitude else { return }
-        
+        Network.shared.getWeatherDataFor(lat: lat, lon: lon) { [weak self] weatherData, error in
+            guard error == nil,
+                  let weatherData = weatherData else { return }
+            self?.weatherData = weatherData
+            self?.updateUI(weatherData)
+        }
+    }
+    
+    func updateUI(_ weatherData: WeatherResponse) {
+        DispatchQueue.main.async { [weak self] in
+            self?.weatherDescriptionLabel.text = weatherData.current?.condition?.text ?? ""
+            self?.weatherTempratureLabel.text = "\(weatherData.current?.tempC ?? 0)"
+            self?.cityNameLabel.text = weatherData.location?.region ?? ""
+        }
     }
 }
+
+
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
